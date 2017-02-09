87121624ec714fa122e857aafad9f748fcaa741a
3elik <3ellik@gmail.com>
2017-02-09 19:31:34 +0400

    change task to use streams

--- gulp/tasks/spritesmith.js
+++ gulp/tasks/spritesmith.js
@@ -2,14 +2,19 @@
 
 module.exports = function() {
   $.gulp.task('sprite:img', function() {
-    return $.gulp.src('./source/sprite/{*.png, *.jpg}')
+    var spriteData =  $.gulp.src('./source/sprite/{*.png, *.jpg}')
       .pipe($.gp.spritesmith({
         imgName: 'sprite.png',
-        cssName: '../style/common/sprite.scss',
+        cssName: 'sprite.scss',
         padding: 5,
         algorithm: 'binary-tree',
         imgPath: '../../assets/img/sprite.png'
-      }))
-      .pipe($.gulp.dest('source/images'));
+      }));
+    var imgStream = spriteData.img
+      .pipe($.gulp.dest('./source/images'));
+    var cssStream = spriteData.css
+      .pipe($.gulp.dest('./source/style/common'));
+
+    return $.merge(imgStream, cssStream);
   });
 };
