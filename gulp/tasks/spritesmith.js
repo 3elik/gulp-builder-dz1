577cfcc712587614748e62529669636484564266
3elik <3ellik@gmail.com>
2017-02-09 19:12:09 +0400

    add the spritesmith task

---
+++ gulp/tasks/spritesmith.js
@@ -0,0 +1,15 @@
+'use  strict';
+
+module.exports = function() {
+  $.gulp.task('sprite:img', function() {
+    return $.gulp.src('./source/sprite/{*.png, *.jpg}')
+      .pipe($.gp.spritesmith({
+        imgName: 'sprite.png',
+        cssName: '../style/common/sprite.scss',
+        padding: 5,
+        algorithm: 'binary-tree',
+        imgPath: '../../assets/img/sprite.png'
+      }))
+      .pipe($.gulp.dest('source/images'));
+  });
+};
